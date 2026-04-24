26a2c0cadee19c6497521aee36ec20247dad172a
AmerBelhadj <146180759+AmerBelhadj@users.noreply.github.com>
2026-04-24 23:19:19 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
    - Tuiles carte : cache-first dynamique
 ══════════════════════════════════════════════════════ */
 
-const CACHE_VERSION = 'jerbi-v3.17.0'; // fix nav overflow clip
+const CACHE_VERSION = 'jerbi-v3.18.0'; // fix nav sticky
 const CACHE_STATIC  = `${CACHE_VERSION}-static`;
 const CACHE_DYNAMIC = `${CACHE_VERSION}-dynamic`;
 
