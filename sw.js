9a801791dea457235e2e1db43474e593be0f585f
AmerBelhadj <146180759+AmerBelhadj@users.noreply.github.com>
2026-04-25 10:32:41 +0200

    Update CACHE_VERSION to jerbi-v3.18.3

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
    - Tuiles carte : cache-first dynamique
 ══════════════════════════════════════════════════════ */
 
-const CACHE_VERSION = 'jerbi-v3.18.2'; // fix nav sticky
+const CACHE_VERSION = 'jerbi-v3.18.3'; // fix nav sticky
 const CACHE_STATIC  = `${CACHE_VERSION}-static`;
 const CACHE_DYNAMIC = `${CACHE_VERSION}-dynamic`;
 
