eab17d043e518b01df96b539ba34615ebb4c21a3
AmerBelhadj <146180759+AmerBelhadj@users.noreply.github.com>
2026-04-30 12:12:35 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
    - Tuiles carte : cache-first dynamique
 ══════════════════════════════════════════════════════ */
 
-const CACHE_VERSION = 'jerbi-v3.18.8'; // fix chatbot window.APP_CONFIG
+const CACHE_VERSION = 'jerbi-v3.18.9'; // fix chatbot window.APP_CONFIG
 const CACHE_STATIC  = `${CACHE_VERSION}-static`;
 const CACHE_DYNAMIC = `${CACHE_VERSION}-dynamic`;
 
